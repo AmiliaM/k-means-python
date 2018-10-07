9ad224e1d959c34dc2235eb0bce8daa4b93d6195
AmiliaM <amiliamac@gmail.com>
2018-10-07 18:56:13 -0400

    works with csv file, multi-dimensional data

--- k_means.py
+++ k_means.py
@@ -2,15 +2,18 @@ import random
 import numpy
 import operator
 import csv
+import pprint
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+
 
 n_means = 3 #the amount of clusters
 means = [] #list containing n_means amount of Mean objects
 n_samples = 50 #amount of test data points - replace with real data
 samples = []
-data_dimensions = 2
+data_dimensions = 14
+n_iters = 0
 
 class Mean: #object to store one mean/centroid
     def __init__(self, pos):
@@ -25,22 +28,44 @@ def nearest_mean(x): #returns mean object closest to given point
         distances[mean] = numpy.linalg.norm(mean.pos - x) 
     return min(distances.items(), key=operator.itemgetter(1))[0] 
 
-def update_mean(): #assigns the means a new pos based on the centroid of its corresponding cluster (where the magic happens)
+def update_mean(): #assigns the means a new pos based on the centroid of its corresponding cluster
     for mean in means:
         mean.prev_points = mean.points
         for i in range(data_dimensions):
-            mean.pos[i] =  sum(map(lambda x: x[i], mean.points))/len(mean.points)
-            mean.pos[i] = sum(map(lambda x: x[i], mean.points))/len(mean.points)
-        
+            try:
+                mean.pos[i] = sum(map(lambda x: x[i], mean.points))/len(mean.points)
+            except:
+                print("Warning: mean has no points at itr ", n_iters)
+
 
 def main():
+    global n_iters
     for _ in range(n_means):
-        means.append(Mean(numpy.array([random.randint(1, 100), random.randint(1, 100)]))) #Randomly generates initial coordinates for Means
+        m = []
+        for _ in range(data_dimensions):
+            m.append(random.randint(1, 100))
+        means.append(Mean(numpy.array(m))) #Randomly generates initial coordinates for Means
     colors = cm.rainbow(numpy.linspace(0, 1, len(means)))
     for i, c in enumerate(means):
         c.color: colors[i]
-    samples = [[random.randint(1, 100), random.randint(1, 100)] for _ in range(n_samples)] #Randomly generate samples    
+
+    #Samples data structure:
+        #List containing n lists containing d integers, where n is the number of samples, and d is the number of dimensions in the data
     
+    #Use csv to read each line of a csv file into samples[]
+
+
+    #samples = [[random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)] for _ in range(n_samples)] #Randomly generate samples    
+    
+    samples = []
+
+    with open('wine.data', 'rt') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            row = [float(i) for i in row]
+            samples.append(row)
+    
+
     print("Successfully generated sample list:")
     print(samples)
     fit = False
@@ -53,10 +78,10 @@ def main():
             update_mean()
         else:
             update_mean()
+            n_iters += 1
     for mean in means:
         print("mean generation finished:")
         print(mean.points)
-        print(" ")
 
     #plotting test
     for i, c in enumerate(means):
